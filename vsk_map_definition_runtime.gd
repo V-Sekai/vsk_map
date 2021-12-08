0cc307eb9a25fc03d4f28bc3f8fe8ccaced02e80
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-12-08 13:02:35 -0800

    Update preview_type.

--- vsk_map_definition_runtime.gd
+++ vsk_map_definition_runtime.gd
@@ -12,7 +12,8 @@ class EntityInstance extends Resource:
 @export var entity_instance_properties_list: Array # (Array) = []
 
 @export var database_id: String
-@export var vskeditor_preview_type: String
+@export_enum("VSK_PREVIEW_CAMERA", "VSK_PREVIEW_TEXTURE") var vskeditor_preview_type: int
+
 @export var vskeditor_preview_texture : Texture2D
 @export_node_path(Camera3D) var vskeditor_preview_camera_path
 @export var vskeditor_pipeline_paths : Array[NodePath]
