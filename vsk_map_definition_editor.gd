ceba7e55b34af56260b5a8dca64cb4dc153966bc
SaracenOne <SaracenOne@gmail.com>
2022-02-18 11:40:04 +0000

    Fix leaks, correct UI skinning

--- vsk_map_definition_editor.gd
+++ vsk_map_definition_editor.gd
@@ -3,10 +3,7 @@ extends Control
 const vsk_types_const = preload("res://addons/vsk_importer_exporter/vsk_types.gd")
 const map_callback_const = preload("res://addons/vsk_map/map_callback.gd")
 
-var editor_plugin : EditorPlugin = null
-
 var node : Node = null
-var options : MenuButton = null
 var err_dialog : AcceptDialog = null
 
 var save_dialog : FileDialog = null
@@ -16,6 +13,7 @@ const OUTPUT_SCENE_EXTENSION = "scn"
 const MENU_OPTION_EXPORT_MAP=0
 const MENU_OPTION_UPLOAD_MAP=1
 
+var editor_plugin: EditorPlugin = null
 
 func export_map_local() -> void:
 	save_dialog.add_filter("*.%s;%s" % [OUTPUT_SCENE_EXTENSION, OUTPUT_SCENE_EXTENSION.to_upper()]);
@@ -43,7 +41,6 @@ func export_map_upload() -> void:
 	
 func edit(p_node : Node) -> void:
 	node = p_node
-	
 	
 func error_callback(p_err: int) -> void:
 	if p_err != map_callback_const.MAP_OK:
@@ -87,36 +84,34 @@ func _save_file_at_path(p_string : String) -> void:
 		p_string)
 		
 	error_callback(err)
-
-func _notification(what):
-	match what:
-		NOTIFICATION_PREDELETE:
-			if editor_plugin:
-				editor_plugin.remove_control_from_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU, options)
-
-func _init(p_editor_plugin : EditorPlugin):
-	editor_plugin = p_editor_plugin
-	
+		
+func setup_dialogs() -> void:
 	err_dialog = AcceptDialog.new()
-	add_child(err_dialog, true)
+	editor_plugin.get_editor_interface().get_base_control().add_child(err_dialog)
 	
 	save_dialog = FileDialog.new()
 	save_dialog.mode = FileDialog.FILE_MODE_SAVE_FILE
 	save_dialog.access = FileDialog.ACCESS_FILESYSTEM
 	save_dialog.exclusive = true
 	save_dialog.connect("file_selected", Callable(self, "_save_file_at_path"))
-	add_child(save_dialog, true)
+	editor_plugin.get_editor_interface().get_base_control().add_child(save_dialog)
 	
-	options = MenuButton.new()
-	options.set_switch_on_hover(true)
+func teardown_dialogs() -> void:
+	if err_dialog:
+		if err_dialog.is_inside_tree():
+			err_dialog.get_parent().remove_child(err_dialog)
+		err_dialog.queue_free()
+		
+	if save_dialog:
+		if save_dialog.is_inside_tree():
+			save_dialog.get_parent().remove_child(err_dialog)
+		save_dialog.queue_free()
 	
-	p_editor_plugin.add_control_to_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU, options)
-	options.set_text("Map Definition")
-	options.get_popup().add_item("Export Map", MENU_OPTION_EXPORT_MAP)
-	options.get_popup().add_item("Upload Map", MENU_OPTION_UPLOAD_MAP)
+func _enter_tree():
+	setup_dialogs()
 	
-	options.get_popup().connect("id_pressed", Callable(self, "_menu_option"))
-	options.hide()
-	
-func _ready():
-	pass
+func _exit_tree():
+	teardown_dialogs()
+		
+func _init(p_editor_plugin: EditorPlugin):
+	editor_plugin = p_editor_plugin
--- vsk_map_plugin.gd
+++ vsk_map_plugin.gd
@@ -7,20 +7,28 @@ const map_definition_const = preload("vsk_map_definition.gd")
 var editor_interface : EditorInterface = null
 var map_definition_editor : Control = null
 
+var option_button: MenuButton = null
 
 func _init():
 	print("Initialising VSKMap plugin")
-
 
 func _notification(p_notification: int):
 	match p_notification:
 		NOTIFICATION_PREDELETE:
 			print("Destroying VSKMap plugin")
 
-
 func _get_plugin_name() -> String:
 	return "VSKMap"
 
+func _menu_option(p_id : int) -> void:
+	if map_definition_editor:
+		map_definition_editor.menu_option(p_id)
+
+func update_menu_options() -> void:
+	if option_button:
+		option_button.get_popup().clear()
+		option_button.get_popup().add_item("Export Map", map_definition_editor_const.MENU_OPTION_EXPORT_MAP)
+		option_button.get_popup().add_item("Upload Map", map_definition_editor_const.MENU_OPTION_UPLOAD_MAP)
 
 func _enter_tree() -> void:
 	editor_interface = get_editor_interface()
@@ -28,14 +36,33 @@ func _enter_tree() -> void:
 	
 	editor_interface.get_viewport().call_deferred("add_child", map_definition_editor, true)
 	
-	map_definition_editor.options.hide()
+	option_button = MenuButton.new()
+	option_button.set_switch_on_hover(true)
+	
+	option_button.set_text("Map Definition")
+	
+	option_button.get_popup().connect("id_pressed", Callable(self, "_menu_option"))
+	option_button.hide()
+	
+	add_control_to_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU, option_button)
 
 func _exit_tree() -> void:
-	map_definition_editor.queue_free()
+	if option_button:
+		remove_control_from_container(EditorPlugin.CONTAINER_SPATIAL_EDITOR_MENU, option_button)
+		if option_button.is_inside_tree():
+			option_button.get_parent().remove_child(option_button)
+		option_button.queue_free()
+	
+	if map_definition_editor:
+		if map_definition_editor.is_inside_tree():
+			map_definition_editor.get_parent().remove_child(map_definition_editor)
+		map_definition_editor.queue_free()
 
 func _edit(p_object : Object) -> void:
-	if p_object is Node and p_object.get_script() == map_definition_const:
-		map_definition_editor.edit(p_object)
+	if map_definition_editor:
+		if p_object is Node and p_object.get_script() == map_definition_const:
+			map_definition_editor.edit(p_object)
+			update_menu_options()
 
 func _handles(p_object : Object) -> bool:
 	if p_object.get_script() == map_definition_const:
@@ -44,12 +71,11 @@ func _handles(p_object : Object) -> bool:
 		return false
 
 func _make_visible(p_visible : bool) -> void:
-	if (p_visible):
-		if map_definition_editor:
-			if map_definition_editor.options:
-				map_definition_editor.options.show()
-	else:
-		if map_definition_editor:
-			if map_definition_editor.options:
-				map_definition_editor.options.hide()
+	if map_definition_editor:
+		if (p_visible):
+			if option_button:
+				option_button.show()
+		else:
+			if option_button:
+				option_button.hide()
 			map_definition_editor.edit(null)
